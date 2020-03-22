934a5db3de83163842743516739987b53d907063
Sinkerine <git@15cm.net>
2020-03-22 13:53:59 -0700

    Update deps

--- make-dict.sh
+++ make-dict.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <regexp for sogou dictionaries>
-Please read the code of $0 for usage :)
-The script makes a sogou dictionary in rime format for luna_pinyin by:
-1. Crawl all sogou |scel| dictionaries.
-2. Filter them by |fd| with the regexp parameter.
-3. Merge them into a single text file."
-  exit 1
-fi
-
 deps=(
   fd
   parallel
   python2
+  dotnet
 )
 
 for dep in ${deps[@]}; do
@@ -23,6 +14,16 @@ for dep in ${deps[@]}; do
     exit 1
   fi
 done
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <regexp for sogou dictionaries>
+Recommend executing '\d\d.*sh' separately. Please read the code of $0 for details.
+The script makes a sogou dictionary in rime format for luna_pinyin by:
+1. Crawl all sogou |scel| dictionaries.
+2. Filter them by |fd| with the regexp parameter.
+3. Merge them into a single text file."
+  exit 1
+fi
 
 ./00-crawl.sh
 ./01-filter-and-convert.sh
